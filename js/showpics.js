e9afc5620c806408235eb2f34335288be0e1af58
QM36 <18791767622@163.com>
2018-10-20 00:02:35 +0800

    修改搜索Bug

--- js/showpics.js
+++ js/showpics.js
@@ -1,16 +1,28 @@
-function getpics () {
-	key = $('.input').html();
+(function () {
+
+var key = window.location.search.match(/(?!u)(key=)([^&]+)/)[2];
+key = decodeURIComponent(key);
+console.log("搜索关键词是："+key);
+getpics(key);
+
+$("#search").click(function() {
+	key = $("#input")[0].value;
+	key = decodeURIComponent(key);
+	console.log("搜索关键词是："+key);
+	getpics(key);
+});
+function getpics (key) {
 	$.ajax(
 	{
 		url: "https://www.easy-mock.com/mock/5bc87a74f6421702c6455d6d/amcs/getallurl",
 		type: "GET",
-		data:key,
-		success: function (data) {
+		data:`key=${key}`,
+		success: function (response) {
 			var html = "";
-			for(var i = 0; i < data.data.length; i++) {
+			for(var i = 0; i < response.data.length; i++) {
 			html += `<div class="col-md-4 col-sm-6 ">
 		                    <div class="box">
-		                        <img src=${data.data[i].url}>
+		                        <img src=${response.data[i].url}>
 		                        <div class="box-content">
 		                            <div class="box-inner-content">
 		                                <h3 class="title">Williamson</h3>
@@ -24,7 +36,8 @@ function getpics () {
 		                    </div>
 		                </div>`
 			}
-			console.log(data);
+			console.log(response);
+			$(".row").empty();
 			$(".row").append(html);
 		},
 		error: function () {
@@ -33,11 +46,6 @@ function getpics () {
 	});
 }
 
-$("#search").click(function(){
-   getpics();
-});
-
-
-
+})();
 
 
