c3e9fcc4351a37c15f5e3a98f167601f1502cd2f
QM36 <18791767622@163.com>
2018-10-20 15:01:27 +0800

    完善readme

--- js/searchpics.js
+++ js/searchpics.js
@@ -1,4 +1,7 @@
-$("#button").click(function() {
-	var url = `show.html?key=${$("#input")[0].value}`;
-	window.location.href = url;
-});
+(function (argument) {
+	$("#button").click(function() {
+		var url = `show.html?key=${$("#input")[0].value}`;
+		window.location.href = url;
+	});
+})();
+
